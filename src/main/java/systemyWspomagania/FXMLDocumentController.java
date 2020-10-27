a79fbfc093cf1f869b8495bdd0b5e4056d1a6c2e
Tomasz Kołtun <tomaszkoltun@MBP-Tomasz.elpos.net>
2020-10-27 20:27:38 +0100

    debugged 2d and 3d scatter plots and added histogram

--- src/main/java/systemyWspomagania/FXMLDocumentController.java
+++ src/main/java/systemyWspomagania/FXMLDocumentController.java
@@ -3,10 +3,10 @@ package systemyWspomagania;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.ResourceBundle;
-
-
+import java.util.Set;
 
 import executePackage.ExecuteFirst;
 import javafx.collections.FXCollections;
@@ -28,11 +28,13 @@ import javafx.stage.FileChooser;
 import javafx.util.Callback;
 import models.Data;
 import tech.tablesaw.api.DoubleColumn;
+import tech.tablesaw.api.IntColumn;
 import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.api.Table;
 import tech.tablesaw.plotly.Plot;
 import tech.tablesaw.plotly.api.Scatter3DPlot;
 import tech.tablesaw.plotly.api.ScatterPlot;
+import tech.tablesaw.plotly.api.VerticalBarPlot;
 
 public class FXMLDocumentController implements Initializable{
 	
@@ -102,6 +104,71 @@ public class FXMLDocumentController implements Initializable{
     private Button draw3dPlotButton;
     
     @FXML
+    private TextField histogramTextField;
+
+    @FXML
+    private Button histogramButton;
+
+    @FXML
+    void createHistogram(ActionEvent event) {
+
+    	String columnId = histogramTextField.getText().toString();
+    	System.out.println("breakpoint");
+    	List<String> dataFromColumn = new ArrayList<>();
+    	int counter = 0;
+    	boolean isNum =isDouble(columnId);
+    	for(Data d : dataToPopulate) { //im taking only the column selected from filtered/%tented dataset
+    		if(counter == 0 && headerCheckbox.isSelected()) {
+    			counter++;
+    			continue;
+    		}
+    		if(columnId.equals("n")) { //user selected a normalized column 
+    			dataFromColumn.add(String.valueOf(d.getNormalizedDataValue()));
+    		}
+    		else { //user provided a column id from dataset
+    			dataFromColumn.add(String.valueOf(d.getData()[Integer.valueOf(columnId)]));
+    			
+    		}
+    	}
+    		
+    		//now im creating a unique values set from the list 
+    		Set<String> uniqueValues = new HashSet<>(dataFromColumn);
+    		int [] occurencesCounter = new int [uniqueValues.size()];
+    		int ctr=0;
+    		//now i have unique values set and now i will create a list sized as this set and count occurences in the columnlist
+    		for(String value : uniqueValues) { //iterate thorugh a set
+    			
+    			for(int i=0;i<dataFromColumn.size();i++) { //iterating through a full set (a column)
+    				if(value.equals(dataFromColumn.get(i))){ //if a value from set equals with the raw column i add 1 to the corresponding space in array
+    					occurencesCounter[ctr]++; //added one
+    				}
+    			}
+    			ctr++;
+    		//here set is full of unique vals and occurences counter has amount of occurences on the same index as in the set
+    		}
+    		String[] setArray = new String[uniqueValues.size()]; //string array from set
+    		setArray = uniqueValues.toArray(setArray);
+    		
+    		
+        	Table myData =
+        		    Table.create("barchart data")
+        		        .addColumns(
+        		            StringColumn.create("first", setArray),
+        		            IntColumn.create("second", occurencesCounter));
+        	
+        	  Plot.show(
+        		        VerticalBarPlot.create(
+        		            "histogram", // plot title
+        		            myData, // table
+        		            "first", // grouping column name
+        		            "second")); // numeric column name
+
+    		
+    		
+    	
+    }
+    
+    @FXML
     void draw2dplot(ActionEvent event) {
 
     	String [] first = new String [dataToPopulate.size()];
@@ -117,7 +184,7 @@ public class FXMLDocumentController implements Initializable{
 	    	}
 	    	else {
 	    		int option=Integer.valueOf(xVal);
-	    		second[i]=String.valueOf(dataToPopulate.get(i).getData()[option]);
+	    		first[i]=String.valueOf(dataToPopulate.get(i).getData()[option]);
 	    	}
 	    	if(yVal.equals("n")) {
 	    		second[i]=String.valueOf(dataToPopulate.get(i).getNormalizedDataValue());
@@ -157,7 +224,7 @@ public class FXMLDocumentController implements Initializable{
 	    	}
 	    	else {
 	    		int option=Integer.valueOf(xVal);
-	    		second[i]=String.valueOf(dataToPopulate.get(i).getData()[option]);
+	    		first[i]=String.valueOf(dataToPopulate.get(i).getData()[option]);
 	    	}
 	    	
 	    	
@@ -279,7 +346,6 @@ public class FXMLDocumentController implements Initializable{
     	ObservableList<Data> allData = FXCollections.observableArrayList();
 
     	
-    	
     	int counter = 0;
     	for(Data d : rawDataset) {
     		if(headerCheckbox.isSelected() && counter ==0 ) {
@@ -320,6 +386,17 @@ public class FXMLDocumentController implements Initializable{
 //    			ScatterPlot.create("2D Plot", 
 //    		                       myData, "zero", "firsr")); //2dplot
     }
+    
+	public boolean isDouble( String input ) {
+	    try {
+	        Double.parseDouble( input );
+	        return true;
+	    }
+	    catch( NumberFormatException e ) {
+	    	System.err.println("column can't be casted to decimal number");
+	        return false;
+	    }
+	}
 
 
 	

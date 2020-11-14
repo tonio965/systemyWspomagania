06ee3644596f936d11db943379ef10d303ba2c1d
tonio965 <tonio96@o2.pl>
2020-11-14 16:01:55 +0100

    main mainly refactored - only histogram and diagrams left to do

--- src/main/java/systemyWspomagania/FXMLDocumentController.java
+++ src/main/java/systemyWspomagania/FXMLDocumentController.java
@@ -90,39 +90,6 @@ public class FXMLDocumentController implements Initializable, DataSender{
 
     @FXML
     private AnchorPane ParentPane;
-
-    @FXML
-    private Button SelectFileBtn;
-
-    @FXML
-    private CheckBox headerCheckbox;
-
-    @FXML
-    private CheckBox minAndMaxValueCheckbox;
-
-    @FXML
-    private TextField maxValueTextField;
-
-    @FXML
-    private CheckBox percentCheckbox;
-
-    @FXML
-    private TextField resultsPercentTextField;
-
-    @FXML
-    private Button firstExcerciseButton;
-
-    @FXML
-    private CheckBox topPercentCheckbox;
-
-    @FXML
-    private TextField minValueTextField;
-    
-    @FXML
-    private TextField columnIdTextField;
-
-    @FXML
-    private TextField sectorsTextField;
     
     @FXML
     private TableView TableView1;
@@ -134,24 +101,27 @@ public class FXMLDocumentController implements Initializable, DataSender{
     @FXML
     private ScatterChart<?, ?> scatterChart1;
     
+    
+    
+    ///zostaje
     @FXML
     private TextField scatterplotXtextField;
-
+    ///zostaje
     @FXML
     private TextField scatterplotYextField;
-
+    ///zostaje
     @FXML
     private TextField scatterplotZextField;
-
+    ///zostaje
     @FXML
     private Button draw2dPlotButton;
-
+    ///zostaje
     @FXML
     private Button draw3dPlotButton;
-    
+    ///zostaje
     @FXML
     private TextField histogramTextField;
-
+    ///zostaje
     @FXML
     private Button histogramButton;
     
@@ -259,10 +229,10 @@ public class FXMLDocumentController implements Initializable, DataSender{
     	int counter = 0;
     	boolean isNum =isDouble(columnId);
     	for(Data d : dataToPopulate) { //im taking only the column selected from filtered/%tented dataset
-    		if(counter == 0 && headerCheckbox.isSelected()) {
-    			counter++;
-    			continue;
-    		}
+//    		if(counter == 0 && headerCheckbox.isSelected()) {
+//    			counter++;
+//    			continue;
+//    		}
     		if(columnId.equals("n")) { //user selected a normalized column 
     			dataFromColumn.add(String.valueOf(d.getNormalizedDataValue()));
     		}
@@ -401,83 +371,24 @@ public class FXMLDocumentController implements Initializable, DataSender{
     //onclick on select etc
     
 
-    @FXML
-    void executeFirstExcercise(ActionEvent event) {
-    	System.out.println("execute first exc");
-    	String path = selectedFile.getAbsolutePath();
-    	boolean headers = headerCheckbox.isSelected();
-    	boolean minMaxCheckbox = minAndMaxValueCheckbox.isSelected(); //ticked original else - min and max
-    	Double minValue= 0d;
-    	Double maxValue= 0d;
-    	boolean showPercentOfResults = percentCheckbox.isSelected(); //selected - original not selected - use topBottomValue
-    	boolean topOrBottomPercentOfResults = topPercentCheckbox.isSelected(); //selected - top not selected - bottom
-    	int percentValue=0;
-    	if(!resultsPercentTextField.getText().equals("")) {
-    		percentValue = Integer.valueOf(resultsPercentTextField.getText());
-    	}
-    	int columnId=0;
-    	if(!columnIdTextField.getText().equals("")) {
-    		columnId = Integer.valueOf(columnIdTextField.getText());
-    	}
-    	int sectorAmount =0;
-    	if(!sectorsTextField.getText().equals("")) {
-    		sectorAmount=Integer.valueOf(sectorsTextField.getText());
-    	}
-    	
-    	
-    	
-    	if(!minValueTextField.getText().equals("")) {
-    		minValue= Double.valueOf(minValueTextField.getText());
-    	}
-    	if(!maxValueTextField.getText().equals("")) {
-    		maxValue= Double.valueOf(maxValueTextField.getText());
-    	}
-    	
-    	
-    	ExecuteFirst executeFirst = new ExecuteFirst();
-    	dataToPopulate=executeFirst.returnDiscretizedAndNormalizedData(path, headers, minMaxCheckbox, minValue, maxValue, 
-    			showPercentOfResults, topOrBottomPercentOfResults, percentValue, columnId, sectorAmount);
-    	
-    	populateTableViewWithData(dataToPopulate);
-//    	createScatterChart(executeFirst.getRawDataset(),0,1);
-    }
-
 	private void createScatterChart(List<Data> rawDataset, int i, int j) {
 		XYChart.Series dataSeries1 = new XYChart.Series();
 		dataSeries1.setName("Scattered data");
 		int counter=0;
 		for(Data d: rawDataset) {
-			if(counter ==0 && headerCheckbox.isSelected()) {
-				counter++;
-				continue;
-			}
-			else {
+//			if(counter ==0 && headerCheckbox.isSelected()) {
+//				counter++;
+//				continue;
+//			}
+//			else {
 				dataSeries1.getData().add(new XYChart.Data( d.getData()[i], Double.valueOf(d.getData()[j])));
 				counter++;
-			}
+//			}
 		}
 		scatterChart1.getData().add(dataSeries1);
 		
 	}
 
-	@FXML
-    void headerCheckboxSelected(ActionEvent event) {
-
-    }
-
-    @FXML
-    void minAndMaxValueCheckboxSelected(ActionEvent event) {
-
-    }
-    
-    @FXML
-    void selectFileButtonPressed(ActionEvent event) {
-    	FileChooser fileChooser = new FileChooser();
-    	FileChooser.ExtensionFilter extFilterTXT = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.TXT");
-    	fileChooser.getExtensionFilters().addAll(extFilterTXT); 
-    	selectedFile = fileChooser.showOpenDialog(null);
-    	
-    }
     
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -495,22 +406,22 @@ public class FXMLDocumentController implements Initializable, DataSender{
 		
 	}
 	
-    private void populateTableViewWithData(List<Data> rawDataset) {
-    	ObservableList<Data> allData = FXCollections.observableArrayList();
-
-    	
-    	int counter = 0;
-    	for(Data d : rawDataset) {
-    		if(headerCheckbox.isSelected() && counter ==0 ) {
-    			counter++;
-    			continue;
-    		}
-    		else {
-        		allData.add(d); 
-        		counter++;
-    		}
-
-    	}
+//    private void populateTableViewWithData(List<Data> rawDataset) {
+//    	ObservableList<Data> allData = FXCollections.observableArrayList();
+//
+//    	
+//    	int counter = 0;
+//    	for(Data d : rawDataset) {
+//    		if(headerCheckbox.isSelected() && counter ==0 ) {
+//    			counter++;
+//    			continue;
+//    		}
+//    		else {
+//        		allData.add(d); 
+//        		counter++;
+//    		}
+//
+//    	}
 //    	System.out.println("breakpoint");
 //    	TableColumn sectorIdColumn = new TableColumn("sectorid");
 //    	sectorIdColumn.setCellValueFactory(new PropertyValueFactory<>("sectorId"));
@@ -538,7 +449,7 @@ public class FXMLDocumentController implements Initializable, DataSender{
 //    	Plot.show(
 //    			ScatterPlot.create("2D Plot", 
 //    		                       myData, "zero", "firsr")); //2dplot
-    }
+    //}
     
 	public boolean isDouble( String input ) {
 	    try {

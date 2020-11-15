e3b2fccebd04efd09299d532192303a5478cfa87
zwykły tomek <39235399+tonio965@users.noreply.github.com>
2020-11-15 21:16:00 +0100

    refactored 3d chart

--- src/main/java/systemyWspomagania/FXMLCreate3DPlotController.java
+++ src/main/java/systemyWspomagania/FXMLCreate3DPlotController.java
@@ -12,6 +12,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import models.DataColumn;
+import tech.tablesaw.api.StringColumn;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.plotly.Plot;
+import tech.tablesaw.plotly.api.Scatter3DPlot;
+import tech.tablesaw.plotly.api.ScatterPlot;
 
 public class FXMLCreate3DPlotController implements Initializable {
 	
@@ -32,8 +37,44 @@ public class FXMLCreate3DPlotController implements Initializable {
 
     @FXML
     void buttonClick(ActionEvent event) {
+    	String columnTitle = comboBox.getValue().toString();
+    	String column2Title = comboBox2.getValue().toString();
+    	String column3Title = comboBox3.getValue().toString();
+    	
+    	String [] first = new String [listOfCols.get(0).getContents().size()];
+    	String [] second = new String [listOfCols.get(0).getContents().size()];
+    	String [] third = new String [listOfCols.get(0).getContents().size()];
+    	
+    	int columnId = findColumnId(columnTitle);
+    	int column2Id = findColumnId(column2Title);
+    	int column3Id = findColumnId(column3Title);
+    	
+    	for(int i=0; i<listOfCols.get(0).getContents().size();i++) {
+    		
+    		first[i]=String.valueOf(listOfCols.get(columnId).getContents().get(i)); 
+    		second[i]=String.valueOf(listOfCols.get(column2Id).getContents().get(i)); 
+    		third[i]=String.valueOf(listOfCols.get(column3Id).getContents().get(i)); 
+    	}
+    	
+    	Table myData =
+    		    Table.create("Cute data")
+    		        .addColumns(
+    		            StringColumn.create(columnTitle, first),
+    		            StringColumn.create(column2Title, second),
+    		            StringColumn.create(column3Title, third));
 
+    	Plot.show(
+    			Scatter3DPlot.create("3D Plot", 
+    		                       myData, columnTitle, column2Title,column3Title )); //3dplot
     }
+    
+	private int findColumnId(String columnTitle) {
+		for(int i=0;i<listOfCols.size();i++) {
+			if(listOfCols.get(i).getTitle().equals(columnTitle))
+				return i;
+		}
+		return 0;
+	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {

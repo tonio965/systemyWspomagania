ee9c312f8ba5d3e7d14bf06b95615437a769ac23
zwykły tomek <39235399+tonio965@users.noreply.github.com>
2020-11-15 21:06:48 +0100

    refactored 2D plot

--- src/main/java/systemyWspomagania/FXMLCreate2DPlotController.java
+++ src/main/java/systemyWspomagania/FXMLCreate2DPlotController.java
@@ -12,6 +12,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import models.DataColumn;
+import tech.tablesaw.api.StringColumn;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.plotly.Plot;
+import tech.tablesaw.plotly.api.ScatterPlot;
 
 public class FXMLCreate2DPlotController implements Initializable{
 	
@@ -29,8 +33,39 @@ public class FXMLCreate2DPlotController implements Initializable{
 
     @FXML
     void buttonClick(ActionEvent event) {
+    	String columnTitle = comboBox.getValue().toString();
+    	String column2Title = comboBox2.getValue().toString();
+    	
+    	String [] first = new String [listOfCols.get(0).getContents().size()];
+    	String [] second = new String [listOfCols.get(0).getContents().size()];
+    	
+    	int columnId = findColumnId(columnTitle);
+    	int column2Id = findColumnId(column2Title);
+    	
+    	for(int i=0; i<listOfCols.get(0).getContents().size();i++) {
+    		
+    		first[i]=String.valueOf(listOfCols.get(columnId).getContents().get(i)); 
+    		second[i]=String.valueOf(listOfCols.get(column2Id).getContents().get(i)); 
+    	}
+    	
+    	Table myData =
+    		    Table.create("Cute data")
+    		        .addColumns(
+    		            StringColumn.create(columnTitle, first),
+    		            StringColumn.create(column2Title, second));
 
+    	Plot.show(
+    			ScatterPlot.create("2D Plot", 
+    		                       myData, columnTitle, column2Title)); //2dplot
     }
+    
+	private int findColumnId(String columnTitle) {
+		for(int i=0;i<listOfCols.size();i++) {
+			if(listOfCols.get(i).getTitle().equals(columnTitle))
+				return i;
+		}
+		return 0;
+	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
